0cf11aa11ce21314a61b1ef88b3cd30ae4aabf7b
gap0n <gap0n@templatemonster.me>
2017-11-30 16:30:02 +0200

    UPD: added language change functionality

--- src/LabelEditor.jsx
+++ src/LabelEditor.jsx
@@ -1,14 +1,14 @@
 const React = novi.react.React;
 import Trigger from "./editor/Trigger";
 import Header from "./editor/Header";
-
+const messages = novi.language.getDataByKey("novi-plugin-label");
 const EditorItem = {
     trigger: <Trigger/>,
-    tooltip: "Edit Label Text",
+    tooltip: messages.editor.tooltip,
     closeIcon: "submit",
     header: [<Header/>],
     width: 290,
-    title: "Edit Label text"
+    title: messages.editor.title
 };
 
 export default EditorItem;
--- src/novi-label.jsx
+++ src/novi-label.jsx
@@ -2,13 +2,14 @@ const React = novi.react.React;
 const domUtils = novi.utils.dom;
 import LabelEditor from "./LabelEditor";
 import LabelSettings from "./LabelSettings";
-
+const Language = novi.language;
 const Plugin = {
     name: "novi-plugin-label",
     title: "Novi Label",
     description: "Novi Label description",
-    version: "1.0.3",
+    version: "1.0.4",
     dependencies: {
+        novi: "0.8.6"
     },
     defaults: {
         querySelector: "label"
@@ -17,7 +18,13 @@ const Plugin = {
         editor: [LabelEditor],
         settings: <LabelSettings />,
     },
-    excerpt: domUtils.hasNonEmptyTextNodes
+    excerpt: domUtils.hasNonEmptyTextNodes,
+    onLanguageChange: onLanguageChange
 };
-
+function onLanguageChange(plugin){
+   let messages = Language.getDataByKey("novi-plugin-label");
+   plugin.ui.editor[0].title = messages.editor.title;
+   plugin.ui.editor[0].tooltip = messages.editor.tooltip;
+   return plugin;
+}
 novi.plugins.register(Plugin);
